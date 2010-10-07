047ebbf1d1dd4a80011f80d0c9d137787663be17
Chris Heisel <chris@heisel.org>
2010-10-07 14:51:57 -0400

    Added logic from jlilly to traverse subdirs for load path

--- init.el
+++ init.el
@@ -10,3 +10,31 @@
     (load
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
+
+;;; Add emacs folders to load path
+(setq load-path (cons "~/.emacs.d/" load-path))
+(setq load-path (cons "~/.emacs.d/configs/" load-path))
+(setq load-path (cons "~/.emacs.d/vendor/" load-path))
+(setq load-path (cons "~/.emacs.d/elpa/" load-path))
+
+;;; Walk through and load things from the config and vendor dirs
+(defconst emacs-config-dir "~/.emacs.d/configs/")
+(defconst emacs-vendor-dir "~/.emacs.d/vendor/")
+(defun get-subdirs (directory)
+  "Get a list of subdirectories under a given directory"
+  (apply 'nconc (mapcar (lambda (fa)
+                        (and
+                         (eq (cadr fa) t)
+                         (not (equal (car fa) "."))
+                         (not (equal (car fa) ".."))
+                         (list (car fa))))
+                        (directory-files-and-attributes directory))))
+
+(defun add-dirs-to-loadpath (dir-name)
+  "add subdirs of your vendor directory to the load path"
+  (dolist (subdir (get-subdirs dir-name))
+    (setq load-path (cons (concat dir-name subdir) load-path))
+    (message "Added %s to load path" subdir)))
+
+(add-dirs-to-loadpath emacs-vendor-dir)
+(add-dirs-to-loadpath "~/.emacs.d/elpa/")

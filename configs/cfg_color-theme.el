b36f5d6012e953055e58ce903f9087843cf8bbf3
Chris Heisel <chris@heisel.org>
2010-10-07 14:55:56 -0400

    Added color theme and zenburn theme

---
+++ configs/cfg_color-theme.el
@@ -0,0 +1,23 @@
+;; Color Theme 
+(require 'color-theme) 
+(color-theme-initialize)
+
+; good color themes:
+; LightBG
+; - andreas
+; - blipp blopp
+; - high contrast
+;
+; DarkBG
+; - Arjen
+; - Billw
+; - calm forest
+; - charcoal black
+; - clairty and beauty
+; - cooper dark
+; - deep blue
+; - hober
+; - renegade
+; - merbivore
+
+(color-theme-zenburn)

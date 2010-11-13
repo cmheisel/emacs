478ff5f7c8b82f8ab46f76a7017cbbdce80891df
Chris Heisel <chris@heisel.org>
2010-11-13 09:27:43 -0500

    Replace frikin ^M characters

--- configs/cfg_generic.el
+++ configs/cfg_generic.el
@@ -14,3 +14,8 @@
 
 ;; do not make backup files
 (setq make-backup-files nil)
+
+;;Replace all freakin' ^M chars in the current buffer
+(fset 'replace-ctrlms
+   [escape ?< escape ?% ?\C-q ?\C-m return ?\C-q ?\C-j return ?!])
+(global-set-key "\C-cm" 'replace-ctrlms)

062a90ec22d21a19bbdb2c1fd557d0a995be57c2
Chris Heisel <chris@heisel.org>
2010-12-04 15:01:38 -0500

    Removed manual path additions using OSX hack outlined in comment.

--- configs/cfg_generic.el
+++ configs/cfg_generic.el
@@ -4,7 +4,6 @@
 (setq user-temporary-file-directory "/tmp/")
 (setq inhibit-startup-message t)
 
-
 ;; Spaces!
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
@@ -19,3 +18,10 @@
 (fset 'replace-ctrlms
    [escape ?< escape ?% ?\C-q ?\C-m return ?\C-q ?\C-j return ?!])
 (global-set-key "\C-cm" 'replace-ctrlms)
+
+;; DEPRECATED in favor of .profile addition outlined here: http://www.emacswiki.org/emacs/EmacsApp#toc3
+;;Path additions
+;;(when (equal system-type 'darwin)
+;;  (setenv "PATH" (concat "/opt/local/bin:/opt/local/sbin:/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin" (getenv "PATH")))
+;;  (push "/opt/local/bin" exec-path)
+;;  (push "/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin" exec-path))
